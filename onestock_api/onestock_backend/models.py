5e863c637f061c36581f85ea368828a1cd335593
Mohtashim Khan <moto_khan@hotmail.com>
2021-12-10 17:20:57 -0700

    Specifc Stock, Account, Brokerage, OrderForm

--- onestock_api/onestock_backend/models.py
+++ onestock_api/onestock_backend/models.py
@@ -6,3 +6,30 @@ class User(models.Model):
     email = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
     isSuperUser = models.CharField(max_length=1)
+
+class Specific_Stock_History(models.Model):
+    ticker = models.CharField(max_length=100)
+    industry = models.CharField(max_length = 100)
+    netProfit = models.IntegerField()
+    exchange = models.CharField(max_length = 100)
+    amountShares = models.IntegerField()
+    currentValue = models.IntegerField()
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+
+class Account(models.Model):
+    accountValue = models.IntegerField()
+    accountType = models.IntegerField()
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+
+class Brokerage(models.Model):
+    fees = models.IntegerField()
+    perOrderFees = models.IntegerField()
+    currencyConversionRate = models.IntegerField()
+
+class OrderForm(models.Model):
+    typeofFile = models.CharField()
+    numOfOrders = models.IntegerField()
+    dateRequested = models.DateField()
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+
