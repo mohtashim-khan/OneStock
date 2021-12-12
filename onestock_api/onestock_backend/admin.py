10fb0c10738bd58ad413b31f70d2896e16550b64
Alaa Khalil <alaakhalil110@gmail.com>
2021-12-11 17:34:27 -0700

    added motos files to  my files, added routing to majority of pages

--- onestock_api/onestock_backend/admin.py
+++ onestock_api/onestock_backend/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.apps import apps
+from django.contrib.admin.sites import AlreadyRegistered
+
+# Register your models here.
+
+models = apps.get_models()
+for model in models:
+    try:
+        admin.site.register(model)
+    except AlreadyRegistered:
+        pass
