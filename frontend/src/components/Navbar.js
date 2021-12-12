3656a3653bb37bf72f3cf71850615ed35781b215
Alaa Khalil <alaakhalil110@gmail.com>
2021-12-11 18:28:08 -0700

    fixed nav bar using old imports

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,12 +1,8 @@
 import React, {Component} from 'react';
-import {Route, Routes, BrowserRouter as Router} from 'react-router-dom';
+
 
 import "../css/navbar.css"
-import StockOrderHistory from '../Pages/StockOrderHistory'
-import SpecificStockHistory from '../Pages/SpecificStockHistory'
-import OtherAssetHistory from '../Pages/OtherAssetHistory'
-import Home from '../Pages/Home'
-import SignIn from '../SigninPage';
+
 
 const NavBar = () => {
     
@@ -16,7 +12,7 @@ const NavBar = () => {
 
                 <h1>OneStock</h1>
                 <div className="links">
-                <a href="/UserPage">Home</a>
+                <a href="/Home">Home</a>
                 <a href="/SpecificStockHistory">Specific Stock History</a>
                 <a href="/StockOrderHistory">Stock Order History</a>
                 <a href="/OtherAssetHistory">Other Asset History</a>
