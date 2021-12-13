cf5ada8dd0cbfb98a9ea140d7aac0557a0b98ee6
Mohtashim Khan <moto_khan@hotmail.com>
2021-12-13 13:06:03 -0700

    Endpoints for all tables

--- onestock_api/onestock_backend/urls.py
+++ onestock_api/onestock_backend/urls.py
@@ -4,7 +4,43 @@ from . import views
 urlpatterns = [
 
     path('', views.apiOverview, name = "api-overview"),
-    path('getSpecificStockHistory/', views.getSpecificStockOrderHistory, name = "specificStockHistory"),
+    path('BrokeragesGetPost/', views.BrokeragesGetPost.as_view(), name = "BrokeragesGetPost"),
+    path('BrokeragesGetPutPatchDelete/', views.BrokeragesGetPutPatchDelete.as_view(), name = "BrokeragesGetPutPatchDelete"),
+    path('BondsGetPost/', views.BondsGetPost.as_view(), name = "BondsGetPost"),
+    path('BondsGetPutPatchDelete/', views.BondsGetPutPatchDelete.as_view(), name = "BondsGetPutPatchDelete"),
+    path('CashGetPost/', views.CashGetPost.as_view(), name = "CashGetPost"),
+    path('CashGetPutPatchDelete/', views.CashGetPutPatchDelete.as_view(), name = "CashGetPutPatchDelete"),
+    path('CommodotiesGetPost/', views.CommodotiesGetPost.as_view(), name = "CommodotiesGetPost"),
+    path('CommoditiesGetPutPatchDelete/', views.CommoditiesGetPutPatchDelete.as_view(), name = "CommoditiesGetPutPatchDelete"),
+    path('CryptoGetPost/', views.CryptoGetPost.as_view(), name = "CryptoGetPost"),
+    path('CrytpoGetPutPatchDelete/', views.CrytpoGetPutPatchDelete.as_view(), name = "CrytpoGetPutPatchDelete"),
+    path('StockOrdersGetPost/', views.StockOrdersGetPost.as_view(), name = "StockOrdersGetPost"),
+    path('StockOrdersGetPutPatchDelete/', views.StockOrdersGetPutPatchDelete.as_view(), name = "StockOrdersGetPutPatchDelete"),
+    path('RealEstateGetPost/', views.RealEstateGetPost.as_view(), name = "RealEstateGetPost"),
+    path('RealEstatePutPatchDelete/', views.RealEstatePutPatchDelete.as_view(), name = "RealEstatePutPatchDelete"),
+    path('SpecificStockOrderHistoryGetPost/', views.SpecificStockOrderHistoryGetPost.as_view(), name = "SpecificStockOrderHistoryGetPost"),
+    path('SpecificStockOrderHistoryPutPatchDelete/', views.SpecificStockOrderHistoryPutPatchDelete.as_view(), name = "SpecificStockOrderHistoryPutPatchDelete"),
+    path('TotalStockOrderHistoryGetPost/', views.TotalStockOrderHistoryGetPost.as_view(), name = "TotalStockOrderHistoryGetPost"),
+    path('TotalStockOrderHistoryPutPatchDelete/', views.TotalStockOrderHistoryPutPatchDelete.as_view(), name = "TotalStockOrderHistoryPutPatchDelete"),
+    path('UserGetPost/', views.UserGetPost.as_view(), name = "UserGetPost"),
+    path('UserPutPatchDelete/', views.UserPutPatchDelete.as_view(), name = "UserPutPatchDelete"),
+    path('OrderFormGetPost/', views.OrderFormGetPost.as_view(), name = "OrderFormGetPost"),
+    path('OrderFormPutPatchDelete/', views.OrderFormPutPatchDelete.as_view(), name = "OrderFormPutPatchDelete"),
+    path('DividendGetPost/', views.DividendGetPost.as_view(), name = "DividendGetPost"),
+    path('DividendPutPatchDelete/', views.DividendPutPatchDelete.as_view(), name = "DividendPutPatchDelete"),
+    path('AccountGetPost/', views.AccountGetPost.as_view(), name = "AccountGetPost"),
+    path('AccountPutPatchDelete/', views.AccountPutPatchDelete.as_view(), name = "AccountPutPatchDelete"),
+
+
+
+
+
+
+    
+
+
+
+
 
     
 ]
--- onestock_api/onestock_backend/views.py
+++ onestock_api/onestock_backend/views.py
@@ -1,11 +1,15 @@
+from django.db.models.query import QuerySet
 from django.http.response import JsonResponse
 from django.shortcuts import render
 from django.http import JsonResponse
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.serializers import Serializer
 from .serializers import *
 from .models import *
+from rest_framework import status
+from rest_framework import generics
 
 
 
@@ -37,9 +41,109 @@ def apiOverview(request):
     }
     return Response(api_urls)
 
-@api_view(['GET'])
-def getSpecificStockOrderHistory(request):
 
-    history = Specific_Stock_History.objects.all()
-    serializer = Specific_Stock_HistorySerializer(history, many = True, context={'request': request})
-    return Response(serializer.data)
+class BrokeragesGetPost(generics.ListCreateAPIView):
+    queryset = Brokerage.objects.all()
+    serializer_class = BrokerageSerializer
+
+class BrokeragesGetPutPatchDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Brokerage.objects.all()
+    serializer_class = BrokerageSerializer
+
+
+class BondsGetPost(generics.ListCreateAPIView):
+    queryset = Bonds.objects.all()
+    serializer_class = BondsSerializer
+
+class BondsGetPutPatchDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Bonds.objects.all()
+    serializer_class = BondsSerializer
+
+class CashGetPost(generics.ListCreateAPIView):
+    queryset = Cash.objects.all()
+    serializer_class = CashSerializer
+
+class CashGetPutPatchDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Cash.objects.all()
+    serializer_class = CashSerializer
+
+class CommodotiesGetPost(generics.ListCreateAPIView):
+    queryset = Commodities.objects.all()
+    serializer_class = CommoditiesSerializer
+
+class CommoditiesGetPutPatchDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Commodities.objects.all()
+    serializer_class = CommoditiesSerializer
+
+class CryptoGetPost(generics.ListCreateAPIView):
+    queryset = CryptoCurrency.objects.all()
+    serializer_class = CryptoCurrencySerializer
+
+class CrytpoGetPutPatchDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = CryptoCurrency.objects.all()
+    serializer_class = CryptoCurrencySerializer
+
+class StockOrdersGetPost(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+class StockOrdersGetPutPatchDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+class RealEstateGetPost(generics.ListCreateAPIView):
+    queryset = RealEstate.objects.all()
+    serialzer_class = RealEstateSerializer
+
+class RealEstatePutPatchDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = RealEstate.objects.all()
+    serializer_class = RealEstateSerializer
+
+class SpecificStockOrderHistoryGetPost(generics.ListCreateAPIView):
+    queryset = Specific_Stock_History.objects.all()
+    serializer_class = Specific_Stock_HistorySerializer
+
+class SpecificStockOrderHistoryPutPatchDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Specific_Stock_History.objects.all()
+    serializer_class = Specific_Stock_HistorySerializer
+
+class TotalStockOrderHistoryGetPost(generics.ListCreateAPIView):
+    queryset = TotalStockHistory.objects.all()
+    serializer_class = TotalStockHistorySerializer
+
+class TotalStockOrderHistoryPutPatchDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = TotalStockHistory.objects.all()
+    serializer_class = TotalStockHistorySerializer
+
+class UserGetPost(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserPutPatchDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class OrderFormGetPost(generics.ListCreateAPIView):
+    queryset = OrderForm.objects.all()
+    serialzer_class = OrderFormSerilaizer
+
+class OrderFormPutPatchDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = OrderForm.objects.all()
+    serializer_class = OrderFormSerilaizer
+
+class DividendGetPost(generics.ListCreateAPIView):
+    queryset = Dividend.objects.all()
+    serialzer_class = DividendSerializer
+
+class DividendPutPatchDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Dividend.objects.all()
+    serializer_class = DividendSerializer
+
+class AccountGetPost(generics.ListCreateAPIView):
+    queryset = Account.objects.all()
+    serialzer_class = AccountSerializer
+
+class AccountPutPatchDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
