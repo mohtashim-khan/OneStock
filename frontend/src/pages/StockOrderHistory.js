4dc720d4d399af7a2c2ee0bfe63badbc6b13f2aa
Alaa Khalil <alaakhalil110@gmail.com>
2021-12-13 14:02:22 -0700

    added the first get request

--- frontend/src/pages/StockOrderHistory.js
+++ frontend/src/pages/StockOrderHistory.js
@@ -1,12 +1,51 @@
-import React from 'react'
+//import React, { Component } from 'react';
+import NavBar from '../components/Navbar';
+import "../css/StockOrderHistory.css"
+import React, { useEffect,useState } from 'react';
 
 
+    const value = 'hello';
+    const [entry,setentry] = useState(null);
+    const query = '?userID=1';
+    useEffect(() => {
+        // GET request using fetch inside useEffect React hook
+        fetch('https://localhost:4000/api/GetStockOrderHistory'+query)
+            .then(response => response.json())
+            .then(data => setentry(data.total));
+    
+    // empty dependency array means this effect will only run once (like componentDidMount in classes)
+    }, []);
 
-function StockOrderHistory(){
-    return(
-        <>
-            <div>
-                <h2>Stock Order History</h2>
+    return (
+        <div className="content">
+            <NavBar />
+            <h2>Stock Order History</h2>
+            <div1>{entry}</div1>
+            <div className="StockOrderHistory">
+                <div className="buttonsContainer">
+                    <button>Import From PC</button>
+                    <button> Create Custom Order</button>
+                    <button>Modify Order By ID</button>
+                    <button>Delete Order By ID</button>
+                    <button>Delete Order by OrderReqID</button>
+                </div>
+                
+
+
+                <table val="11">
+                    <caption>Order History</caption>
+
+                    <tr>
+                        <th>Order ID</th>
+                        <th>Ticker</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th>Date</th>
+                        <th>Value</th>
+                        <th>OrderReqID</th>
+                        <th>Account</th>
+                    </tr>
+                </table>
             </div>
         </>
     );
