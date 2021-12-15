34702985c4185c53c566fbbb3f4af181ffd69118
Alaa Khalil <alaakhalil110@gmail.com>
2021-12-15 16:42:11 -0700

    added other asset table,orders added buy sell modl

--- frontend/src/pages/OtherAssetHistory.js
+++ frontend/src/pages/OtherAssetHistory.js
@@ -3,10 +3,10 @@
 import NavBar from '../components/Navbar';
 import React, { useEffect,useState } from 'react';
 function OtherAssetHistory(){
-    const [entrycrypto,setCrypto] = useState(null);
-    const [entrycash,setCash] = useState(null);
+    const [entrycryptos,setCrypto] = useState(null);
+    const [entrycashs,setCash] = useState(null);
     const [entrybonds,setBonds] = useState(null);
-    const [entrycommod,setCommod] = useState(null);
+    const [entrycommods,setCommod] = useState(null);
     const [entryreales,setReales] = useState(null);
     useEffect(() => {
         // GET request using fetch inside useEffect React hook
@@ -48,6 +48,18 @@ function OtherAssetHistory(){
                         <th>Purchase Price($)</th>
                         <th>Quantity</th>
                     </tr>
+                    {entrycryptos&&
+                        entrycryptos.map((entrycrpyto) => (
+                            <tr>
+                                <th>{entrycrpyto.id}</th>
+                                <th>{entrycrpyto.valuation}</th>
+                                <th>{entrycrpyto.name}</th>
+                                <th>{entrycrpyto.purchasePrice}</th>
+                                <th>{entrycrpyto.quantity}</th>
+                                
+                            </tr>
+                        ))
+                    }
                 </table>
                 <table val = "7">
                     <caption>Commodities</caption>
@@ -57,16 +69,38 @@ function OtherAssetHistory(){
                         <th>Type Of Commodities</th>
                         
                     </tr>
+                    {entrycommods&&
+                        entrycommods.map((entrycommod) => (
+                            <tr>
+                                <th>{entrycommod.id}</th>
+                                <th>{entrycommod.valuation}</th>
+                                <th>{entrycommod.type}</th>
+                                
+                            </tr>
+                        ))
+                    }
                 </table>
                 <table val = "8">
                     <caption>Bond</caption>
                     <tr>
                         <th scope="col">AssetID</th>
                         <th>Valuation($)</th>
-                        <th>Face Value($)</th>
+                        <th>Principal Value($)</th>
                         <th>Maturity Date</th>
                         <th>Interest</th>
                     </tr>
+                    {entrybonds&&
+                        entrybonds.map((entrybond) => (
+                            <tr>
+                                <th>{entrybond.id}</th>
+                                <th>{entrybond.valuation}</th>
+                                <th>{entrybond.principal}</th>
+                                <th>{entrybond.maturityDate}</th>
+                                <th>{entrybond.interest}</th>
+                                
+                            </tr>
+                        ))
+                    }
                 </table>
                 <table val = "9">
                     <caption>Real Estate</caption>
@@ -77,6 +111,17 @@ function OtherAssetHistory(){
                         <th>Location</th>
                        
                     </tr>
+                    {entryreales&&
+                        entryreales.map((entryreale) => (
+                            <tr>
+                                <th>{entryreale.id}</th>
+                                <th>{entryreale.valuation}</th>
+                                <th>{entryreale.type}</th>
+                                <th>{entryreale.location}</th>
+                                
+                            </tr>
+                        ))
+                    }
                 </table>
                 <table val = "10">
                     <caption>Cash</caption>
@@ -87,6 +132,17 @@ function OtherAssetHistory(){
                         <th>Type of Currency</th>
                         
                     </tr>
+                    {entrycashs&&
+                        entrycashs.map((entrycash) => (
+                            <tr>
+                                <th>{entrycash.id}</th>
+                                <th>{entrycash.valuation}</th>
+                                <th>{entrycash.bank}</th>
+                                <th>{entrycash.currency}</th>
+                                
+                            </tr>
+                        ))
+                    }
                 </table>
                 
                 <group>
