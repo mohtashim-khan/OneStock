9ee29944a8f3eabbb15db9b916f176c8d6d72fa8
Mohtashim Khan <moto_khan@hotmail.com>
2021-12-18 00:55:15 -0700

    1255

--- frontend/src/pages/SpecificStockHistory.js
+++ frontend/src/pages/SpecificStockHistory.js
@@ -4,9 +4,9 @@ import NavBar from '../components/Navbar';
 import React, { useEffect, useState } from 'react';
 import axiosInstance from '../axios';
 function SpecificStockHistory() {
-    const [entryorders, setOrder] = useState(null);
+    const [orders, setOrders] = useState(null);
     const [entrydividend, setDividend] = useState(null);
-    const [specificStock, setStock] = useState(null);
+    const [specificStockHistory, setStockHistory] = useState(null);
     const [ticker, setTicker] = useState('');
 
 
@@ -26,9 +26,9 @@ function SpecificStockHistory() {
     const handleSubmit = (e) => {
 
         axiosInstance
-            .get('SpecificStockOrderHistoryPutPatchDeleteByTicker/' + ticker+'/')
+            .get('SpecificStockOrderHistoryPutPatchDeleteByTicker/' + ticker + '/')
             .then(response => {
-                setStock(response.data)
+                setStockHistory(response.data);
             })
             .catch((err) => {
                 console.log(err)
@@ -39,23 +39,11 @@ function SpecificStockHistory() {
             );
 
 
-        axiosInstance
-            .get('DividendGetPost/?format=json&ticker=' + ticker)
-            .then(response => {
-                setDividend(response.data)
-            })
-            .catch((err) => {
-                console.log(err)
-                alert("permission denied");
-            }
-
-
-            );
 
         axiosInstance
-            .get('StockGetPost/?format=json&ticker=' + ticker)
+            .get('StockOrdersGetPost/')
             .then(response => {
-                setStock(response.data)
+                setOrders(response.data);
             })
             .catch((err) => {
                 console.log(err)
@@ -71,60 +59,50 @@ function SpecificStockHistory() {
     return (
         <>
 
-        
+
             <NavBar />
             <group id="specific">
-            <h2>Specific Stock Information</h2>
-            <form class="form-wrapper" onSubmit={handleSubmit}>
-                
-                    
-                <input type="text" placeholder="Enter Specific Ticker e.g TSLA.." onChange={(e) => setTicker(e.target.value)}></input>
-                <input type="submit" name="submit" value="Search" /> 
-                
-                
-            </form>
-            <p2> Specific Stock Performance: </p2>
-            <net_back>%3.9</net_back>
-            <p3>Stock Net Value:</p3>
-            <over_back>$1,221.02</over_back>
-            <table val = "1">
-                <caption>Dividend Information</caption>
-                <tr>
-                    <th scope="col">Ticker</th>
-                    <th>Dividend Yield(%)</th>
-                    <th>Dividend Last Paid($)</th>
-                    <th>Total Accumulated($)</th>
-                    <th>Account</th>
-                </tr>
-            </table>
-            <table val = "3">
-                <caption>Shares Information</caption>
-                <tr>
-                    <th scope="col">Ticker</th>
-                    <th>Average Purchase Price($)</th>
-                    <th>Current Price($)</th>
-                    <th>Shares Owned</th>
-                    <th>Account</th>
-                </tr>
-            </table>
-            <table val = "4">
-                <caption>Order History</caption>
-                <tr>
-                    <th scope="col">Buy/Sell</th>
-                    <th>Order ID</th>
-                    <th>Amount</th>
-                    <th>Purchase/Sell Price($)</th>
-                    <th>Date</th>
-                    <th>Account</th>
-                </tr>
-            </table>
-           
+                <h2>Specific Stock Information</h2>
+                <form class="form-wrapper" onSubmit={handleSubmit}>
 
-            
+
+                    <input type="text" placeholder="Enter Specific Ticker e.g TSLA.." onChange={(e) => setTicker(e.target.value)}></input>
+                    <input type="submit" name="submit" value="Search" />
+
+
+                </form>
+                <p2> Specific Stock Performance: </p2>
+                <net_back>%3.9</net_back>
+                <p3>Stock Net Value:</p3>
+                <over_back>$1,221.02</over_back>
+                <table val="3">
+                    <caption>Shares Information</caption>
+                    <tr>
+                        <th scope="col">Ticker</th>
+                        <th>Average Purchase Price($)</th>
+                        <th>Current Price($)</th>
+                        <th>Shares Owned</th>
+                        <th>Account</th>
+                    </tr>
+                </table>
+                <table val="4">
+                    <caption>Order History</caption>
+                    <tr>
+                        <th scope="col">Buy/Sell</th>
+                        <th>Order ID</th>
+                        <th>Amount</th>
+                        <th>Purchase/Sell Price($)</th>
+                        <th>Date</th>
+                        <th>Account</th>
+                    </tr>
+                </table>
+
+
+
 
             </group>
 
-        
+
         </>
     );
 }
